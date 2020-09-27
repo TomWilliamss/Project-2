50e5d0c8c595326c3d872845644c01f5c52e5f47
TomWilliamss <48627746+TomWilliamss@users.noreply.github.com>
2020-09-27 15:28:42 -0400

    Added comments

--- project2.m
+++ project2.m
@@ -4,7 +4,7 @@ A = imread("match1.gif");
 B = imread("match2.gif");
 
 % Testing the custom operators
-imtool(customAND(A,B))
-imtool(customOR(A,B))
-imtool(customXOR(A,B))
-imtool(customNOT(A))
+imtool(customAND(A,B)) %intersection of A and B
+imtool(customOR(A,B)) %union of A and B
+imtool(customXOR(A,B)) %(~AnB)u(An~B)
+imtool(customNOT(A)) %Not A (~A)
